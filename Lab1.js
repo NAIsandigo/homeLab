740954c1ac7911d8b103c81ef2082d11a1abc2bf
NAIsandigo <31249574+NAIsandigo@users.noreply.github.com>
2017-09-25 08:55:30 -0500

    Add files via upload
    commit js file.

---
+++ Lab1.js
@@ -0,0 +1,194 @@
+var motorbikes = [
+ {
+   "Name": "Honda",
+   "countryOfOrigin": "Japan",
+   "modelName": "CB400",
+   "engineSize": "408 cc",
+   "productionStart": 1975,
+   "productionEnd": 1977
+ },
+ {
+   "Name": "Yamaha",
+   "countryOfOrigin": "Japan",
+   "modelName": "RD400",
+   "engineSize": "399 cc",
+   "productionStart": 1975,
+   "productionEnd": 1980
+ },
+ {
+   "Name": "Suzuki",
+   "countryOfOrigin": "Japan",
+   "modelName": "GT550",
+   "engineSize": "543 cc",
+   "productionStart": 1972,
+   "productionEnd": 1977
+ },
+ {
+   "Name": "Kawasaki",
+   "countryOfOrigin": "Japan",
+   "modelName": "Ninja 500R",
+   "engineSize": "498 cc",
+   "productionStart": 1987,
+   "productionEnd": 2009
+ },
+ {
+   "Name": "Harley Davidson",
+   "countryOfOrigin": "USA",
+   "modelName": "XR-750",
+   "engineSize": "748 cc",
+   "productionStart": 1970,
+   "productionEnd": 1985
+ },
+ {
+   "Name": "Royal Enfield",
+   "countryOfOrigin": "Britain",
+   "modelName": "500 Twin",
+   "engineSize": "500 cc",
+   "productionStart": 1948,
+   "productionEnd": 1960
+ },
+ {
+   "Name": "Indian",
+   "countryOfOrigin": "USA",
+   "modelName": "Chief",
+   "engineSize": "1300 cc",
+   "productionStart": 1950,
+   "productionEnd": 1953
+ },
+ {
+   "Name": "Ducati",
+   "countryOfOrigin": "Italy",
+   "modelName": "860 GT",
+   "engineSize": "863.9 cc",
+   "productionStart": 1974,
+   "productionEnd": 1979
+ },
+ {
+   "Name": "BMW",
+   "countryOfOrigin": "Germany",
+   "modelName": "K-100",
+   "engineSize": "987 cc",
+   "productionStart": 1982,
+   "productionEnd": 1992
+ },
+ {
+   "Name": "Triumph",
+   "countryOfOrigin": "Britain",
+   "modelName": "T110 Tiger",
+   "engineSize": "649 cc",
+   "productionStart": 1953,
+   "productionEnd": 1961
+ },
+ {
+   "Name": "Moto Guzzi",
+   "countryOfOrigin": "Italy",
+   "modelName": "850 Le Mans",
+   "engineSize": "844 cc",
+   "productionStart": 1976,
+   "productionEnd": 1992
+ },
+ {
+   "Name": "Norton",
+   "countryOfOrigin": "Britain",
+   "modelName": "650SS",
+   "engineSize": "646 cc",
+   "productionStart": 1962,
+   "productionEnd": 1967
+ },
+ {
+   "Name": "Buell",
+   "countryOfOrigin": "USA",
+   "modelName": "S1 Lightning",
+   "engineSize": "1203 cc",
+   "productionStart": 1996,
+   "productionEnd": 1999
+ },
+ {
+   "Name": "Victory",
+   "countryOfOrigin": "USA",
+   "modelName": "Gunner",
+   "engineSize": "1731 cc",
+   "productionStart": 2014,
+   "productionEnd": 2016
+ },
+ {
+   "Name": "Husqvarna",
+   "countryOfOrigin": "Austria",
+   "modelName": "B31",
+   "engineSize": "348 cc",
+   "productionStart": 1945,
+   "productionEnd": 1959
+ },
+ {
+   "Name": "Hero",
+   "countryOfOrigin": "India",
+   "modelName": "Karizama R",
+   "engineSize": "223 cc",
+   "productionStart": 2003,
+   "productionEnd": 2017
+ },
+ {
+   "Name": "TVS",
+   "countryOfOrigin": "India",
+   "modelName": "GLX 125",
+   "engineSize": "124.8 cc",
+   "productionStart": 2004,
+   "productionEnd": 2004
+ },
+ {
+   "Name": "Benelli",
+   "countryOfOrigin": "Italy",
+   "modelName": "Tornado TRE 900",
+   "engineSize": "898 cc",
+   "productionStart": 2002,
+   "productionEnd": 2014
+ },
+ {
+   "Name": "AJS",
+   "countryOfOrigin": "Britain",
+   "modelName": "7R",
+   "engineSize": "348 cc",
+   "productionStart": 1948,
+   "productionEnd": 1954
+ },
+ {
+   "Name": "Hyosung",
+   "countryOfOrigin": "South Korea",
+   "modelName": "A50",
+   "engineSize": "498 cc",
+   "productionStart": 1962,
+   "productionEnd": 1970
+ },
+ {
+   "Name": "sommer",
+   "countryOfOrigin": "Germany",
+   "modelName": "Diesel 462",
+   "engineSize": "462 cc",
+   "productionStart": 2015,
+   "productionEnd": 2015
+ },
+ {
+   "Name": "BSA",
+   "countryOfOrigin": "Britain",
+   "modelName": "Lightning Rocket",
+   "engineSize": "654 cc",
+   "productionStart": 1964,
+   "productionEnd": 1970
+ },
+ {
+   "Name": "KTM",
+   "countryOfOrigin": "Austria",
+   "modelName": "Duke 620",
+   "engineSize": "609 cc",
+   "productionStart": 1995,
+   "productionEnd": 1996
+ },
+ {
+   "Name": "Jawa",
+   "countryOfOrigin": "Czech Republic",
+   "modelName": "350 TS",
+   "engineSize": "343 cc",
+   "productionStart": 1987,
+   "productionEnd": 1989
+ } 
+]
